d8d203f1f049475138d92a118c20d32e0154b64a
Maxim <turbo3.6@cn.ru>
2020-08-23 12:44:13 +0700

    Updates

---
+++ precache-manifest.185cc84d182e8624b409ba66fa7327ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a28852b432bee399d52977d0df7237",
+    "url": "/melivaro/igHome/index.html"
+  },
+  {
+    "revision": "79b8e9731a1dec818056",
+    "url": "/melivaro/igHome/static/css/main.b7c37be9.chunk.css"
+  },
+  {
+    "revision": "40f22fb33ed5da845a1e",
+    "url": "/melivaro/igHome/static/js/2.6a15e0bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/melivaro/igHome/static/js/2.6a15e0bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b8e9731a1dec818056",
+    "url": "/melivaro/igHome/static/js/main.76532daa.chunk.js"
+  },
+  {
+    "revision": "31e88f69fabb62de2333",
+    "url": "/melivaro/igHome/static/js/runtime-main.8876cb07.js"
+  }
+]);
